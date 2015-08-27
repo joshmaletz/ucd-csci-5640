8d19ab5ce797a5a0a28e2f619556413f8735187e
Josh Maletz <josh.j.maletz@nordstrom.com>
2015-08-27 17:38:58 -0600

    get parse tree printing working

--- Compiler/MicroParser.Domain.Tests/ParseTreeShould.cs
+++ Compiler/MicroParser.Domain.Tests/ParseTreeShould.cs
@@ -11,22 +11,96 @@ namespace MicroParser.Domain.Tests
     [TestFixture]   
     public class ParseTreeShould
     {
+        public ParseTreeShould()
+        {
+        }
+
+        [Test]
+        public void AllowNestedOutputAtEachLevel2()
+        {
+            var tree = new ParseTree("<system goal>");
+            tree.AddTerminalChild("begin");
+            //tree.AddChild(BuildStatement());
+            tree.AddChildNode(BuildStatementNode());
+            tree.AddTerminalChild("end");
+            tree.BuildLinesDifferently();
+            Assert.AreEqual("<system goal>", tree.Lines[0]);
+            Assert.AreEqual("begin <statement> end ", tree.Lines[1]);
+            Assert.AreEqual("begin Id := <expression> ; end ", tree.Lines[2]);
+            Assert.AreEqual("begin Id := <primary> <add op> <expression> ; end ", tree.Lines[3]);
+            Assert.AreEqual("begin Id := Id <add op> <expression> ; end ", tree.Lines[4]);
+            Assert.AreEqual("begin Id := Id PlusOp <expression> ; end ", tree.Lines[5]);
+            Assert.AreEqual("begin Id := Id PlusOp <primary> ; end ", tree.Lines[6]);
+            Assert.AreEqual("begin Id := Id PlusOp IdX ; end ", tree.Lines[7]);
+        }
+
         [Test]
         public void AllowNestedOutputAtEachLevel()
         {
             var tree = new ParseTree("<system goal>");
-            tree.AddChild("begin");
-            tree.AddChild(BuildStatement());
-            tree.AddChild("end");
+            tree.AddTerminalChild("begin");
+            //tree.AddChild(BuildStatement());
+            tree.AddChildNode(BuildStatementNode());
+            tree.AddTerminalChild("end");
             tree.BuildLines();
             Assert.AreEqual("<system goal>", tree.Lines[0]);
-            Assert.AreEqual("begin <statement> end", tree.Lines[1]);
+            Assert.AreEqual("begin <statement> end ", tree.Lines[1]);
+            Assert.AreEqual("begin Id := <expression> ; end ", tree.Lines[2]);
+            Assert.AreEqual("begin Id := <primary> <add op> <expression> ; end ", tree.Lines[3]);
+            Assert.AreEqual("begin Id := Id <add op> <expression> ; end ", tree.Lines[4]);
+            Assert.AreEqual("begin Id := Id PlusOp <expression> ; end ", tree.Lines[5]);
+            Assert.AreEqual("begin Id := Id PlusOp <primary> ; end ", tree.Lines[6]);
+            Assert.AreEqual("begin Id := Id PlusOp IdX ; end ", tree.Lines[7]);
         }
 
         private TreeNode<string> BuildStatementNode()
         {
-            return new TreeNode<string>("<statement>");
+            var statementNode = new TreeNode<string>("<statement>");
+            statementNode.AddTerminalChild("Id");
+            statementNode.AddTerminalChild(":=");
+            statementNode.AddChildNode(BuildExpressionNode());
+            statementNode.AddTerminalChild(";");
+            return statementNode;
         }
+
+        private TreeNode<string> BuildExpressionNode()
+        {
+            var expressionNode = new TreeNode<string>("<expression>");
+
+            expressionNode.AddChildNode(BuildFirstPrimaryIdNode());
+            expressionNode.AddChildNode(BuildAddOpNode());
+            expressionNode.AddChildNode(BuildPrimaryExpression());
+
+            return expressionNode;
+        }
+
+        private TreeNode<string> BuildPrimaryExpression()
+        {
+            var firstPrimary = new TreeNode<string>("<expression>");
+            var terminalPrimary = new TreeNode<string>("<primary>");
+            terminalPrimary.AddTerminalChild("IdX");
+            firstPrimary.AddChildNode(terminalPrimary);
+
+            return firstPrimary;
+        }
+
+        private TreeNode<string> BuildAddOpNode()
+        {
+            var addOp = new TreeNode<string>("<add op>");
+
+            addOp.AddTerminalChild("PlusOp");
+
+            return addOp;
+        } 
+
+        private TreeNode<string> BuildFirstPrimaryIdNode()
+        {
+            var firstPrimary = new TreeNode<string>("<primary>");
+
+            firstPrimary.AddTerminalChild("Id");
+
+            return firstPrimary;
+        } 
 
         private string BuildStatement()
         {
--- Compiler/MicroParser.Domain/ParseTree.cs
+++ Compiler/MicroParser.Domain/ParseTree.cs
@@ -16,6 +16,114 @@ namespace MicroParser.Domain
             
         }
 
+        public void BuildLinesDifferently()
+        {
+            this.Lines.Add(this.Item);
+            this.Lines.Add(string.Format(GetChildLine(this)));
+            this.Template = "{0}";
+            EveryNonTermChildHasOneLine(this.Template, this);
+        }
+
+        private string GetChildLine(TreeNode<string> node)
+        {
+            string line = string.Empty;
+            foreach (var child in node.Children)
+            {
+                line += child.Item;
+                line += " ";
+            }
+
+            return line;
+        }
+
+        private void EveryNonTermChildHasOneLine(string template, TreeNode<string> node)
+        {
+            string line = GetChildLine(node);
+            
+            // for every non term child of the node, create one line
+            //this.Lines.Add(string.Format(template, line));
+
+           
+
+            // now line as is should be added.
+            List<string> linesToGenerate = new List<string>();
+            Dictionary<int, int> nonTerminalMap = new Dictionary<int, int>();
+            int countOfNonTerminalsInChildren = 0;
+            int index = 0;
+            foreach (var child in node.Children)
+            {
+                if (!child.IsTerminal)
+                {
+                    nonTerminalMap.Add(countOfNonTerminalsInChildren, index);
+                    countOfNonTerminalsInChildren++;
+                }
+
+                index++;
+            }
+
+            for (int nonTerminalIndex = 0; nonTerminalIndex < countOfNonTerminalsInChildren; nonTerminalIndex++)
+            {
+                string nextLineTemplate = string.Empty;
+
+                // build template lines 1 at a time..
+                line = string.Empty;
+                string childLine = string.Empty;
+                
+                for(int childIndex = 0; childIndex < node.Children.Count; childIndex++)
+                //foreach (var child in node.Children)
+                {
+                    var child = node.Children[childIndex];
+                    if (child.IsTerminal)
+                    {
+                        nextLineTemplate += child.Item;
+                        nextLineTemplate += " ";
+                    }
+                    else if (!child.IsTerminal && child.HasChildren())
+                    {
+                        var currentNonTerminal = nonTerminalMap[nonTerminalIndex];
+                        if (childIndex == currentNonTerminal)
+                        {
+                            //countOfSubs++;
+                            //digDeeper = true;
+                            nextLineTemplate += "{0}";
+                            //nextLineTemplate += " ";
+                            childLine = GetChildLine(child);
+                            //AddLine(child, nextLineTemplate);
+                        }
+                        else
+                        {
+                            if (child.HasBeenHandled)
+                            {
+                                nextLineTemplate += GetChildLine(child);
+                                //nextLineTemplate += " ";
+                            }
+                            else
+                            {
+                                nextLineTemplate += child.Item;
+                                nextLineTemplate += " ";
+                            }
+                        }
+                    }
+                }
+
+                //string wholeTemplate = template;
+                string wholeTemplate = string.Format(template, nextLineTemplate);
+                //string childLine = GetChildLine(node);
+                this.Lines.Add(string.Format(wholeTemplate, childLine));
+
+                node.Children[nonTerminalMap[nonTerminalIndex]].Template = wholeTemplate;
+                node.Children[nonTerminalMap[nonTerminalIndex]].HasBeenHandled = true;
+            }
+
+            foreach (var child in node.Children)
+            {
+                if (!child.IsTerminal)
+                {
+                    EveryNonTermChildHasOneLine(child.Template, child);
+                }
+            }
+        }
+
         public void BuildLines()
         {
             this.Lines.Add(this.Item);
@@ -23,40 +131,91 @@ namespace MicroParser.Domain
             // build a recursive output by walking the tree.
             // each line should build by all outer nodes included in output
 
-            AddLine(this);
+            AddLine(this, "{0}");
         }
 
-        private void AddLine(TreeNode<string> node)
+        private void AddLine(TreeNode<string> node, string template)
         {
             string line = string.Empty;
+            string nextLineTemplate = string.Empty;
+            bool digDeeper = false;
+            int countOfSubs = 0;
             foreach (var child in node.Children)
             {
                 line += child.Item;
                 line += " ";
-                //if (!child.HasChildren())
-                //{
-
-                //}
-                //else
-                //{
-
-                //}
+                if (child.IsTerminal)
+                {
+                    nextLineTemplate += child.Item;
+                    nextLineTemplate += " ";
+                }
+                else if(!child.IsTerminal && child.HasChildren())
+                {
+                    if (countOfSubs == 0)
+                    {
+                        countOfSubs++;
+                        digDeeper = true;
+                        nextLineTemplate += "{0}";
+                        //nextLineTemplate += " ";
+                        //AddLine(child, nextLineTemplate);
+                    }
+                    else
+                    {
+                        nextLineTemplate += child.Item;
+                        nextLineTemplate += " ";
+                    }
+                }
             }
-            this.Lines.Add(line);
+
+            this.Lines.Add(string.Format(template, line));
+
+            // now we should have created next line template if there was a non terminal
+            if (digDeeper == true)
+            {
+                foreach (var child in node.Children)
+                {
+                    if (!child.IsTerminal && child.HasChildren())
+                    {
+                        AddLine(child, string.Format(template, nextLineTemplate));
+                    }
+                }
+            }
+
+
+            
         }
+
+        
     }
 
     public class TreeNode<T>
     {
         public List<TreeNode<T>> Children;
 
-        public T Item { get; set; }
+        public void AddChildNode(TreeNode<T> buildStatementNode)
+        {
+            this.Children.Add(buildStatementNode);
+        }
 
+        public T Item { get; set; }
+        public string Template { get; set; }
+        public bool HasBeenHandled { get; set; }
         public TreeNode(T item)
         {
             Item = item;
             Children = new List<TreeNode<T>>();
         }
+
+        public bool IsTerminal { get; private set; }
+
+        public TreeNode<T> AddTerminalChild(T item)
+        {
+            
+            var nodeItem = this.AddChild(item);
+            nodeItem.IsTerminal = true;
+
+            return nodeItem;
+        } 
 
         public TreeNode<T> AddChild(T item)
         {
